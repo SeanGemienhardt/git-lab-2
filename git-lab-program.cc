4019ed1b9c0ae06cb429eb04c2187513d266b1fe
Sean Gemienhardt <sg330618@ohio.edu>
2021-01-26 19:57:36 -0500

    git lab file

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Sean Gemienhardt
+ *        Date: 26-1-2021
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    return 0;
+}// main
